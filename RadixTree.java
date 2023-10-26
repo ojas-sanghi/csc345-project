88442c8815cd08f5efbde05d9a9953415163f90b
Jake Balla <gamemasteru03@gmail.com>
2023-10-25 23:27:39 -0700

    Get Rid of Edge Helper Class
    Makes more consistent with Tri implementation, may make inserting a bit more difficult though

--- RadixTree.java
+++ RadixTree.java
@@ -5,32 +5,18 @@ public class RadixTree {
     private class RadixNode {
 
         boolean isEnd;
-//        private HashMap<Character,Edge> edges;
-        private Edge [] edges;
-        int edgeSize;
+        private RadixNode [] children;
+        int childrenSize;
+        String label;
 
         RadixNode() {
             // TODO: change this, don't need 26
             // maybe also change in standard trie? make it a dynamic list?
             // can change to more memory efficient one once we have tests at a later point
-            // This needs to be a hash map since each edge can be a substring of unkown size
-            edges = new Edge[26];
-            edgeSize = 0;
+            label = "";
+            children = new RadixNode [26];
+            childrenSize = 0;
             isEnd = false;
-        }
-    }
-    private class Edge{
-        String label;
-        RadixNode next;
-        Edge(String label, RadixNode next){
-            this.label = label;
-            if(next == null){
-                next = new RadixNode();
-                next.isEnd = true;
-            }
-            else {
-                next = new RadixNode();
-            }
         }
     }
 
@@ -54,7 +40,7 @@ public class RadixTree {
     private RadixNode delete(RadixNode curr, String word){
         // Base Case
         if(word.isEmpty()){ // Base case when all characters are gone
-            if(curr.edgeSize == 0 && curr != root){ // Go ahead and null out if not the root and nothing inside
+            if(curr.childrenSize == 0 && curr != root){ // Go ahead and null out if not the root and nothing inside
                 return null;
             }
             curr.isEnd = false; // Else only pathway to other words, no longer ending
@@ -66,26 +52,27 @@ public class RadixTree {
         // Verification that it actually exists
         char c = Character.toLowerCase(word.charAt(0));
         int index = Character.charCount(c) - 97;
-        Edge edge = curr.edges[index]; // Gets the edge which points to the next node
-        if(edge == null || ! word.startsWith(edge.label)){ // We can do this since we know that there will be at most one substring with that char
+        RadixNode child = curr.children[index];
+        if(child == null || ! word.startsWith(child.label)){ // We can do this since we know that there will be at most one substring with that char
             return curr; // Does not exist so don't modify
         }
         // Deletion
-        RadixNode deleted = delete(edge.next, word.substring(edge.label.length())); // Recursive call onto next node with remianing substring
+        RadixNode deleted = delete(child, word.substring(child.label.length())); // Recursive call onto next node with remianing substring
         // Handle case where it now points to nothing
         if(deleted == null){ // Point to nothing, then remove edge that points to it!
-            curr.edgeSize -= 1;
-            curr.edges[index] = null;
-            if(curr.edgeSize == 0 && !curr.isEnd && curr != root){ // This node is now useless since doesn't point to ending or next word!
+            curr.childrenSize -= 1;
+            curr.children[index] = null;
+            if(curr.childrenSize == 0 && !curr.isEnd && curr != root){ // This node is now useless since doesn't point to ending or next word!
                 return null;
             }
         }
         // Node with only one next, hence lets just merge the two together
-        else if(!deleted.isEnd && deleted.edgeSize == 1){ // Just an empty node pointing to only one edge
-            deleted.edgeSize -= 1;
-            for(int i = 0; i < deleted.edges.length; i ++){ // Right now we have to loop through to find removal Edge, pehaps can be made O(1)
-                if(deleted.edges[i] != null){
-                    curr.edges[index] = new Edge(edge.label + deleted.edges[i].label, deleted.edges[i].next);
+        else if(!deleted.isEnd && deleted.childrenSize == 1){ // Just an empty node pointing to only one edge
+            deleted.childrenSize -= 1;
+            for(int i = 0; i < deleted.children.length; i ++){ // Right now we have to loop through to find removal Edge, pehaps can be made O(1)
+                if(deleted.children[i] != null){
+                    deleted.children[i].label = child.label + deleted.children[i].label; // Plop in new label
+                    curr.children[index] = deleted.children[i]; // Point past empty node
                 }
             }
         }
